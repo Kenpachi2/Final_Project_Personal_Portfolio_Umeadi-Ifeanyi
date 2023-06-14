9c95778ebec1675e40f4ade45ec5664f24726d6b
Kenpachi2 <ifeanyiumeadi201@gmail.com>
2023-06-14 08:11:25 -0700

    push project files

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// Leaflet Map
+
+var map = L.map('map').setView([6.211521774221579, 7.071387911336021], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
